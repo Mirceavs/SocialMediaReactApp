ce947bf653e5db30461050505040638f97b659e9
Mirceavs <mircea236@gmail.com>
2023-07-16 14:41:54 +0300

    user registration function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,3 +2,39 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
+// Register user 
+
+export const register = async(req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User ({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 1000), // dummy value, not wirting the functionality for the sake of simplicity
+            impressions: Math.floor(Math.random() * 1000), // same as above
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (error) {
+        res.status(500).json({ error: err.message });
+    }
+};
