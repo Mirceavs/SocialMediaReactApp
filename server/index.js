e1329a808befd1c814ce34b15c9a235bfc818eae
Mirceavs <mircea236@gmail.com>
2023-07-17 15:53:23 +0300

    GET and PATCH routes added

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import multer from 'multer';
 import path from "path";
 import { fileURLToPath } from 'url';
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 // Configurations
@@ -45,6 +46,7 @@ app.post("/auth/register" , upload.single("picture"), register);
 // Routes 
 
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 // Mongoose configuration
 
