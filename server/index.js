3ecfdf138243fe6bb6fd8e435b76078db6d88be6
Mirceavs <mircea236@gmail.com>
2023-07-17 12:37:27 +0300

    user login function

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import morgan from 'morgan';
 import multer from 'multer';
 import path from "path";
 import { fileURLToPath } from 'url';
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 // Configurations
@@ -38,8 +39,12 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 // Authentication
-//Routes with files
+// Routes with files
 app.post("/auth/register" , upload.single("picture"), register);
+
+// Routes 
+
+app.use("/auth", authRoutes);
 
 // Mongoose configuration
 
