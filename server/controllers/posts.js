92d05ac48c77caa7970b68e5708d70eaf30cd23f
Mirceavs <mircea236@gmail.com>
2023-07-29 01:25:01 +0300

    Add controllers responsible for handling the READ operation for post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -21,12 +21,29 @@ export const createPost = async (req, res) => {
         await newPost.save();
 
         const post = await Post.find();
-
         res.status(201).json(post);
     } catch (err) {
-        res.status(409).json({ message: err.message })
+        res.status(409).json({ message: err.message });
     }
 }
 
 // READ 
 
+export const getFeedPosts = async(req, res) => {
+    try {
+        const post = await Post.find();
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(404).json({ message: "Failed to fetch posts" });
+    }
+}
+
+export const getUserPosts = async(req, res) => {
+    try {
+        const { userId } = req.params;
+        const post = await Post.find({ userId });
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(404).json({ message: "Failed to fetch user post" });
+    }
+}
