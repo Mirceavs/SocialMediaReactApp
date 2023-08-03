ad421ed579c573da8717a2cf0abdcf4bab731082
Mirceavs <mircea236@gmail.com>
2023-08-03 15:39:36 +0300

    Add FlexBetween component/ persistConfig fixed/ Add ThemeProvider|CssBaseline wrapper

--- client/src/index.js
+++ client/src/index.js
@@ -18,17 +18,17 @@ import {
 import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 
-const persistConfig = { key: "root", storage, version: 1};
+const persistConfig = { key: "root", storage, version: 1,};
 const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
-  reducer: persistReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
   getDefaultMiddleware({
     serializableCheck: {
       ignoreActions: [FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER]
-    }
-  })
-})
+    },
+  }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
