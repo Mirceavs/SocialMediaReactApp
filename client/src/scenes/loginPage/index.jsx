89e038de13a53cd9f990bcd2e5421ec6d7aacfe5
Mirceavs <mircea236@gmail.com>
2023-08-04 19:01:08 +0300

    Start loginPage

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,35 @@
-const LoginPage = () => {
-    return (<div>Log In Page</div>)
-}
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.pallete.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          SociolMediaReactApp
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.pallete.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to SocialMediaReactApp!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
 export default LoginPage;
